7b8f958913306fe545fb87da6f744dbe056e5e72
vector <zijiebing@gmail.com>
2021-06-19 16:42:23 +0800

    implement tcp_receiver

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -1,19 +1,37 @@
 #include "tcp_receiver.hh"
-
-// Dummy implementation of a TCP receiver
-
-// For Lab 2, please replace with a real implementation that passes the
-// automated checks run by `make check_lab2`.
-
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
-
 using namespace std;
 
+//! \brief this method needs to set ISN if necessary and push the payload into reassembler.
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    // return if cannot translate
+    const auto &hdr = seg.header();
+    if(!_isn.has_value() && !hdr.syn) return;
+    const auto &pld = seg.payload().copy();
+    const auto ckpt = stream_out().bytes_written();
+    if(hdr.syn) {
+        _isn = hdr.seqno;
+        const uint64_t abs_seqno = unwrap(hdr.seqno+1, _isn.value(), ckpt); // +1 for SYN
+        _reassembler.push_substring(pld, abs_seqno-1, hdr.fin);
+    } else {
+        const uint64_t abs_seqno = unwrap(hdr.seqno, _isn.value(), ckpt);
+        _reassembler.push_substring(pld, abs_seqno-1, hdr.fin);
+    }
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+//! \brief find next seqno that receiver interested in.
+//! \returns WrappingInt32, containing SYN and FIN.
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    const size_t abs_seqno = stream_out().bytes_written() + 1;
+    if(!_isn.has_value()) { // not even start.
+        return {};
+    } else if(!stream_out().input_ended()) { // not yet ended
+        return wrap(abs_seqno, _isn.value());
+    } else { // ended. add 1 for FIN
+        return wrap(abs_seqno, _isn.value()) + 1;
+    }
+}
 
-size_t TCPReceiver::window_size() const { return {}; }
+//! \brief find the distance between first unassembled and first unacceptable.
+//! It's apparent that _capacity = stream_out().buffer_size() + window_size.
+//! so \returns _capacity - stream_out().buffer_size()
+size_t TCPReceiver::window_size() const { return _capacity - stream_out().buffer_size(); }
